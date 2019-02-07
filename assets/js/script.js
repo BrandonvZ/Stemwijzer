91cdd1a881f240aa0046cd34fd256dac96938ede
Brandon van Zanten <brandonvanzanten@gmail.com>
2019-02-07 23:56:52 +0100

    added answered question functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,9 @@ const partyPage = document.getElementById('partypage');
 // all buttons
 const startBtn = document.getElementById('startBtn');
 const backBtn = document.getElementById('backBtn');
+const eensBtn = document.getElementById("Eens");
+const noneBtn = document.getElementById("Geen van beide");
+const oneensBtn = document.getElementById("Oneens");
 
 // all party sides
 var eensList = document.getElementById('eensList');
@@ -29,28 +32,46 @@ var answers = [];
 var partyIsOpen = false;
 
 // this function will be called if you press the start button
-function startQuestion(){
+function StartQuestion(){
     // enables and disables the pages
     startPage.style.display = "none";
     questionPage.style.display = "block";
 
-    // will set the progressbar to the correct percetage
+    // will set the progressbar to the correct percentage
     progressBar.style.width = (100 / subjects.length + 1) + "%";
 
-    // resets the index to 0 and calls the showQuestion function
+    // resets the index to 0 and calls the ShowQuestion function
     index = 0;
-    showQuestion();
+    ShowQuestion();
 }
 
-// this function will display the current question title and information
-function showQuestion(){
+// this function will display the current question title and information and handle answered question
+function ShowQuestion(){
+    // automatically closes dropdown
+    partyPage.style.display = "none";
+    partyIsOpen = false;
+
+    // removes active styling on buttons
+    eensBtn.classList.remove("active");
+    noneBtn.classList.remove("active");
+    oneensBtn.classList.remove("active");
+
+    // if question has already been answered, show answer
+    if(answers.length >= index && answers[index] != undefined && answers[index].answer != ''){
+
+        // get button and apply active class
+        var prevSelectedBtn = document.getElementById(answers[index].answer);
+        prevSelectedBtn.classList.add("active");
+    }
+
     questionTopic.innerHTML = (index + 1) + ". " + subjects[index]['title'];
     questionInformation.innerHTML = subjects[index]['statement'];
-    generateParties();
+
+    GenerateParties();
 }
 
 // this function will sent the user choice
-function nextQuestion(choice){
+function NextQuestion(choice){
     // handle question data into an object
     var list = {
         'title' : subjects[index].title,
@@ -72,13 +93,13 @@ function nextQuestion(choice){
         questionPage.style.display = "none";
         console.log(answers);
     } else {
-        showQuestion();
+        ShowQuestion();
     }
 }
 
 // this function will show the previous question
-function previousQuestion(){
-    // decreases progressbar and index, and gives the progressbar a transition effect
+function PreviousQuestion(){
+    // decreases the progressbar and index, and gives the progressbar a transition effect
     progressBar.style.width = (100 / subjects.length * index) + "%";
     progressbar.style.transition = "all 0.2s";
     index--;
@@ -88,12 +109,12 @@ function previousQuestion(){
         startPage.style.display = "block";
         questionPage.style.display = "none";
     } else {
-        showQuestion();
+        ShowQuestion();
     }
 }
 
 // this function will toggle the parties dropdown
-function toggleDropdown(){
+function ToggleDropdown(){
     // if the page is disabled and the user presses on the dropdown, the page will be enabled. If not, then the other way around.
     if(partyIsOpen == false){
         partyPage.style.display = "block";
@@ -105,40 +126,40 @@ function toggleDropdown(){
 }
 
 // this function will generate all parties and their side
-function generateParties(){
+function GenerateParties(){
     eensList.innerHTML = "";
     noneList.innerHTML = "";
     oneensList.innerHTML = "";
 
     // loop through all parties in the subjects list
     for(var i = 0; i < subjects[index].parties.length; i++){
-        var party = subjects[index].parties;
+        var party = subjects[index].parties[i];
 
         // if a party is for the current statement
-        if(party[i].position == "pro"){
-            eensList.innerHTML += "<div class='party-dropdown-container" + i + "' onclick='togglePartyDropdown(" + i + ");'><p class='party-container-partyname'>" + party[i].name + "</p>";
+        if(party.position == "pro"){
+            eensList.innerHTML += "<div class='party-dropdown-container" + i + "' onclick='togglePartyDropdown(" + i + ");'><p class='party-container-partyname'>" + party.name + "</p>";
             eensList.innerHTML += "<img id='party-dropdown-image" + i + "' value='false' class='party-dropdown-image' src='assets/img/icon-dropdown.svg'>";
-            eensList.innerHTML += "<div id='party-dropdown" + i + "' class='party-container-dropdown'><p>" + party[i].explanation + "</p></div></div>";
+            eensList.innerHTML += "<div id='party-dropdown" + i + "' class='party-container-dropdown'><p>" + party.explanation + "</p></div></div>";
         }
 
         // if a party is neutral the current statement
-        if(party[i].position == "ambivalent"){
-            noneList.innerHTML += "<div class='party-dropdown-container" + i + "' onclick='togglePartyDropdown(" + i + ");'><p class='party-container-partyname'>" + party[i].name + "</p>";
+        if(party.position == "ambivalent"){
+            noneList.innerHTML += "<div class='party-dropdown-container" + i + "' onclick='togglePartyDropdown(" + i + ");'><p class='party-container-partyname'>" + party.name + "</p>";
             noneList.innerHTML += "<img id='party-dropdown-image" + i + "' value='false' class='party-dropdown-image' src='assets/img/icon-dropdown.svg'>";
-            noneList.innerHTML += "<div id='party-dropdown" + i + "' class='party-container-dropdown'><p>" + party[i].explanation + "</p></div></div>";
+            noneList.innerHTML += "<div id='party-dropdown" + i + "' class='party-container-dropdown'><p>" + party.explanation + "</p></div></div>";
         }
 
         // if a party is against the current statement
-        if(party[i].position == "contra"){
-            oneensList.innerHTML += "<div class='party-dropdown-container" + i + "' onclick='togglePartyDropdown(" + i + ");'><p class='party-container-partyname'>" + party[i].name + "</p>";
+        if(party.position == "contra"){
+            oneensList.innerHTML += "<div class='party-dropdown-container" + i + "' onclick='togglePartyDropdown(" + i + ");'><p class='party-container-partyname'>" + party.name + "</p>";
             oneensList.innerHTML += "<img id='party-dropdown-image" + i + "' value='false' class='party-dropdown-image' src='assets/img/icon-dropdown.svg'>";
-            oneensList.innerHTML += "<div id='party-dropdown" + i + "' class='party-container-dropdown'><p>" + party[i].explanation + "</p></div></div>";
+            oneensList.innerHTML += "<div id='party-dropdown" + i + "' class='party-container-dropdown'><p>" + party.explanation + "</p></div></div>";
         }
     }
 }
 
 // this function will hande the dropdown icon and information
-function togglePartyDropdown(i){
+function TogglePartyDropdown(i){
     var dropdownImg = document.getElementById('party-dropdown-image' + i);
     var partyStatement = document.getElementById('party-dropdown' + i);
 
